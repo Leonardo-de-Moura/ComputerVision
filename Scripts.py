4d330252d3f1538aa12ba501da8e38e3bd7a45d0
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2026-01-06 15:32:31 -0300

    up

---
+++ Scripts.py
@@ -0,0 +1,14 @@
+import cv2
+
+init = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+imagem = cv2.imread('dataset/img.png')
+imagemTransmuted = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+faces = init.detectMultiScale(imagemTransmuted)
+
+print(faces)
+
+for(x,y,w,h) in faces:
+    cv2.rectangle(imagem,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv2.imshow("faces", imagem)
+cv2.waitKey()
